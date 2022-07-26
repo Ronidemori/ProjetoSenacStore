fb72fd7fb987d90693e88495292abb9a259920e3
Ronidemori <ronidemori@gmail.com>
2022-07-25 22:18:33 -0300

    atualizacao projeto senac

--- Store/views.py
+++ Store/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from  django.shortcuts import render
+from django.http import HttpResponse
+from Store.models import Departamento
 
 # Create your views here.
+def index(request):
+    meu_nome= ' bertano da silva'
+    sexo = 'M'
+    context = {'nome': meu_nome,'artigo':'o' if sexo == 'M' else 'a'
+    }
+    return render (request,'index.html',context)  
+
+def teste(request):
+   # depto =  ['casa','Informatica','Telefonia'] 
+    depto = Departamento.objects.all() 
+    context = {'departamentos': depto}   
+    return render( request,'teste.html',context)
+    
+    
+   
+
+
+
